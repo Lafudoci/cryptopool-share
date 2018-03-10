aa6023c7929bab622a2af0dccb5cd0715e88e402
Lafudoci <chunhsi.tso@gmail.com>
2018-03-10 14:56:46 +0800

    Add nanopool xmr support

--- checkworker.py
+++ checkworker.py
@@ -1,26 +1,181 @@
 import configparser
+import time
 import requests
 import json
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-def ethtwgpumine():
-	all_shares = 0
-	msg = ''
-	try:
-		api_url='https://eth-tw.gpumine.org/api/miner/'+ config['eth-tw.gpumine.org']['address']   
-		resp = requests.get(url = api_url, timeout = 10)
-		if str(resp) == '<Response [200]>':
-			miners_data = json.loads(resp.text)		
-			return miners_data
-		else:
-			print('API error')
+def apirequest(api_url):
+	i = 0
+	while(i<3):
+		try:    
+			resp = requests.get(url=api_url, timeout = 10)
+			if str(resp) == '<Response [200]>':
+				resp_data = json.loads(resp.text)
+				print(api_url[0:24]+'--> Pool API OK')
+				return resp_data
+			else:
+				print(api_url[0:24]+'--> API error')
+				i += 1
+				return -1
+		except requests.exceptions.RequestException as err:
+			print(api_url[0:24]+'--> Requests error')
+			i += 1
 			return -1
-	except requests.exceptions.RequestException as err:
-		print('Requests error')
+
+
+def calculateCredit(work_credit):
+	all_credit = 0
+	work_share ={}
+
+	for worker,credit in work_credit.items():		# sum workers's credit
+		all_credit += credit
+	print('Credit sum: '+ str(all_credit))
+
+	for worker,credit in work_credit.items():		# calculate workers's share
+		work_share[worker] = credit/all_credit
+
+	print('Shares percentage in this period:\n'+json.dumps(work_share)+'\n')
+
+	return work_share
+
+def checkPay(pool):
+	try:											# read last payment hash from cache
+		lp = open( pool+'_pay', 'r')
+		lastpay = json.loads(lp.read())
+		last_hash = lastpay['Last_hash']
+		lp.close()
+	except (OSError, IOError) as e:
+		print(pool+": Previous payment hash not found")
+		last_hash = 'Not yet'
+
+	if pool == 'ethwgpu':							# get last payment hash from ethwgpu pool
+		api_url='https://eth-tw.gpumine.org/api/miner/'+ config['eth-tw.gpumine.org']['address']   
+
+		miners_data = apirequest(api_url)
+
+
+		pay_hash = miners_data['payments'][-1]['tx_hash']
+		status = miners_data['payments'][-1]['status']
+		paid = miners_data['payments'][-1]['paid']
+		if pay_hash != last_hash and status == 'completed' :
+			print(pool+': New payment was found!: '+ paid)
+			print(pool+': Last payment:\n'+pay_hash)
+			last_hash = pay_hash
+			last_pay = {'Last_hash':str(last_hash),'paid':paid}
+			lp = open(pool+'_pay', 'w')
+			lp.write(json.dumps(last_pay))
+			lp.close()
+			return last_pay
+		else:
+			print(pool+': Nothing new, last payment:\n'+str(last_hash))
+			return -2
+
+	elif pool == 'nanopoolxmr':						# get last payment hash from nanopool
+		api_url='https://api.nanopool.org/v1/xmr/payments/'+ config['xmr.nanopool.org']['address']        
+		miners_data = apirequest(api_url)
+		pay_hash = miners_data['data'][0]['txHash']
+		status = miners_data['data'][-1]['confirmed']
+		paid = miners_data['data'][-1]['amount']
+		if pay_hash != last_hash and status == True :
+			print(pool+': New payment was found!: '+ str(paid))
+			print(pool+': Last payment:\n'+pay_hash)
+			last_hash = pay_hash
+			last_pay = {'Last_hash':str(last_hash),'paid':paid}
+			lp = open(pool+'_pay', 'w')
+			lp.write(json.dumps(last_pay))
+			lp.close()
+			return last_pay
+		else:
+			print(pool+': Nothing new, last payment:\n'+str(last_hash))
+			return -2
+	else:
+		print(pool+': Check payment error')
 		return -1
 
 
+
+def ethtwgpumine():
+	
+	api_url='https://eth-tw.gpumine.org/api/miner/'+ config['eth-tw.gpumine.org']['address']   
+	miners_data = apirequest(api_url)
+
+	try:
+		cd = open('ethtwgpu_credit', 'r')			# read credit from cache
+		work_credit = json.loads(cd.read())
+		cd.close()
+	except (OSError, IOError) as e:
+		print('New recording')
+		work_credit = {}
+
+	msg = ''
+	for worker in miners_data['workers']:			# load worker credit from pool
+		worker_id  = worker['rig']
+		hashrate24h = worker['hashrate24h']
+		shares = worker['validShares']
+		
+		if worker_id not in work_credit:			# add new workers's credit
+			work_credit[worker_id] = shares
+		else:
+			credit = work_credit[worker_id]			# add up old worker's credit
+			work_credit[worker_id] = credit + shares
+
+		msg += ('%s: \t%s, %d shares.\n' % (worker_id, hashrate24h, shares) )
+
+	print(time.strftime("======== %Y-%m-%d %H:%M:%S ========", time.localtime()))
+	print('Real-time status:\n'+msg)
+	
+	cd = open('ethtwgpu_credit', 'w')				# write the credit back to cache
+	cd.write(json.dumps(work_credit))
+	cd.close()
+
+	print('Shares among workers:\n'+json.dumps(work_credit)+'\n')
+
+	return work_credit
+
+
+def nanopoolxmr():
+
+	api_url='https://api.nanopool.org/v1/xmr/workers/'+ config['xmr.nanopool.org']['address']        
+	miners_data = apirequest(api_url)
+
+	try:
+		cd = open('nanopoolxmr_credit', 'r')		# read credit from cache
+		work_credit = json.loads(cd.read())
+		cd.close()
+	except (OSError, IOError) as e:
+		print('New recording')
+		work_credit = {}
+
+	now = time.time()
+	msg = ''
+	for worker in miners_data['data']:				# load worker credit from pool
+		worker_id  = worker['id']
+		hashrate = worker['hashrate']
+		shares = worker['rating']
+		
+		if worker_id not in work_credit:			# add new workers's credit
+			work_credit[worker_id] = hashrate
+		else:
+			credit = work_credit[worker_id]			# add up old worker's credit
+			work_credit[worker_id] = credit + hashrate
+
+		msg += ('%s: \t%s H/s.\n' % (worker_id, hashrate) )
+
+	print(time.strftime("======== %Y-%m-%d %H:%M:%S ========", time.localtime()))
+	print('Real-time status:\n'+msg)
+	
+	cd = open('nanopoolxmr_credit', 'w')			# write the credit back to cache
+	cd.write(json.dumps(work_credit))
+	cd.close()
+
+	print('Total hashs among workers:\n'+json.dumps(work_credit)+'\n')
+
+	return work_credit
+
 if __name__ == '__main__':
-    ethtwgpumine()
+    # ethtwgpumine()
+    nanopoolxmr()
+    # checkPay('ethwgpu')
+    # checkPay('nanopoolxmr')
--- main.py
+++ main.py
@@ -2,108 +2,38 @@ import configparser
 import time
 import requests
 import json
-
 import checkworker
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-last_hash = ''
 work_share = {}
 last_check = 0
 final_payout = {}
-scan_interval = int(config['eth-tw.gpumine.org']['scan_interval'])
-
-
-def calculateCredit(miners_data):
-	all_credit = 0
-	msg = ''
-
-	try:
-		cd = open('credit.json', 'r')				# read credit from cache
-		work_credit = json.loads(cd.read())
-		cd.close()
-	except (OSError, IOError) as e:
-		print('New recording\n')
-		work_credit = {}
-
-	for worker in miners_data['workers']:			# load worker credit from pool
-		worker_id  = worker['rig']
-		hashrate24h = worker['hashrate24h']
-		shares = worker['validShares']
-		
-		if worker_id not in work_credit:			# add new workers's credit
-			work_credit[worker_id] = shares
-		else:
-			credit = work_credit[worker_id]			# add up old worker's credit
-			work_credit[worker_id] = credit + shares
-
-		msg += ('%s: \t%s, %d shares.\n' % (worker_id, hashrate24h, shares) )
-	
-	cd = open('credit.json', 'w')					# write the credit back to cache
-	cd.write(json.dumps(work_credit))
-	cd.close()
-
-	print(time.strftime("\n======== %Y-%m-%d %H:%M:%S ========", time.localtime()))
-	print('Real-time status:\n'+msg)
-
-
-	for worker,credit in work_credit.items():		# calculate workers's share
-		all_credit += credit
-	print('All_credit: '+ str(all_credit))
-
-	for worker,credit in work_credit.items():		# calculate workers's share
-		work_share[worker] = credit/all_credit
-
-	print('Shares among workers:\n'+json.dumps(work_credit)+'\n')
-	print('Shares percentage in this period:\n'+json.dumps(work_share)+'\n')
-
-def checkPay(miners_data):
-	global last_hash
-	try:
-		lp = open('lastpay.json', 'r')
-		lastpay = json.loads(lp.read())
-		last_hash = lastpay['Last_hash']
-		lp.close()
-	except (OSError, IOError) as e:
-		print("Recording last payment hash\n")
-		last_hash = ''
-
-	pay_hash = miners_data['payments'][-1]['tx_hash']
-	status = miners_data['payments'][-1]['status']
-	paid = miners_data['payments'][-1]['paid']
-	if pay_hash != last_hash and status == 'completed' :
-		print('New payment was found!: '+ paid)
-		last_hash = pay_hash
-		lp = open('lastpay.json', 'w')
-		lp.write(json.dumps({'Last_hash':last_hash}))
-		lp.close()
-		return paid
-	else:
-		print('Last payment:\n'+pay_hash)
-		return -1
-
+scan_interval = int(config['general']['scan_interval'])
 
 
 while(True):
 	if ( time.time() - last_check > scan_interval*60):
 		
 		while(True):
-			miners_data = checkworker.ethtwgpumine()
-			if miners_data == -1:
+			work_credit = checkworker.nanopoolxmr()		#get and sum worker credit from nanopool
+			if work_credit == -1:
 				print('Can not get data from pool, will retry in 10s' )
 				time.sleep(10)
 			else:
 				break
 
+		work_share = checkworker.calculateCredit(work_credit)		# calculate workers's share
 
-		calculateCredit(miners_data)
+		payment = checkworker.checkPay('nanopoolxmr')				# check last payment from pool
 
-		payment = checkPay(miners_data)
+		if payment == -2:
+			print('Wait next check in '+str(scan_interval)+' mins')
 
-		if payment != -1:
-			for worker,percent in work_share.items():
-				final_payout[worker] = float(payment) * float(percent)
+		elif payment != -1:
+			for worker,percent in work_share.items():				# divide profit by shares
+				final_payout[worker] = float(payment['paid']) * float(percent)
 			print('Final payout table:\n'+json.dumps(final_payout))
 			loacal_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
@@ -112,7 +42,7 @@ while(True):
 				pay_list += '%s,%.17f\n'%(worker,pay)
 
 			pay_sheet = open('payout_share.csv', 'a')
-			pay_sheet.write('%s,%s\n%s\n' % (loacal_time, last_hash, pay_list))
+			pay_sheet.write('%s,%s\n%s\n' % (loacal_time, payment['Last_hash'], pay_list))
 			
 			pay_sheet.close()
 
@@ -123,4 +53,4 @@ while(True):
 		
 		last_check = time.time()
 
-time.sleep(1)
+time.sleep(5)
